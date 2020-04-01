f52077f80e9036ff5c67f234730516db41725e0c
funncy <gywns41@naver.com>
2020-04-01 23:33:26 +0900

    storages

--- config/settings.py
+++ config/settings.py
@@ -167,3 +167,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/media/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
+STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'selffitting-eb-s3'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_AUTO_CREATE_BUCKET = True
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
