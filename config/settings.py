29808c2a026ec2e8c09faad36aeb30be97abfaa9
funncy <gywns41@naver.com>
2020-04-01 17:36:50 +0900

    selffitting code4

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, "static"), '.static_root')
