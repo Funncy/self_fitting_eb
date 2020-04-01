f5318f585709ee23c4c8229e209c780e4ba407e2
funncy <gywns41@naver.com>
2020-04-01 20:11:10 +0900

    utf8mb4

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         "PORT": os.environ.get("PORT"),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf-8mb4",
         },
     },
 }
